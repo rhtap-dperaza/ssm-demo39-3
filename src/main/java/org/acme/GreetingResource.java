2f31e565231e88a8074f4ed5d367120005c8e07d
David Peraza <dperaza@redhat.com>
2026-02-24 15:46:48 -0500

    Update GreetingResource.java
    DAVP1

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy, from DAVP0";
+        return "Hello RESTEasy, from DAVP1";
     }
 }
